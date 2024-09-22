758501b5f5c3a5389f8c5d406cd58dc4993664ad
Yosef-Ali <yosethio@yahoo.com>
2024-09-22 22:44:13 +0300

    Refactor page titles in dashboard and sales components

--- app/dashboard/orders/new/page.tsx
+++ app/dashboard/orders/new/page.tsx
@@ -3,8 +3,8 @@ import SalesPage from "@/components/SalesPage";
 export default function AddOrderPage() {
   return (
     <>
-      <div className="flex-1 space-y-4 p-8 pt-4">
-        <h2 className="text-2xl font-bold mb-4">Add New Order</h2>
+      <div className="flex-1 space-y-2 px-8">
+        <h2 className="text-2xl font-bold mb-2">Add New Order</h2>
       </div>
       <SalesPage />
     </>
