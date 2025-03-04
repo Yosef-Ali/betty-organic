e65d63a54f59f3afabe9d8ebb15b4bbe7e549de4
Yosef-Ali <yosethio@yahoo.com>
2025-03-04 11:35:27 +0300

    Update social media links in Footer component and add LinkedIn link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -190,17 +190,17 @@ export default function Footer() {
               <h4 className="text-lg font-semibold">Follow Us</h4>
               <div className="flex gap-4">
                 {/* Replace these with your actual social media links */}
-                <a href="https://twitter.com" className="hover:text-yellow-600">
+                <a href="https://tiktok.com" className="hover:text-yellow-600">
                   <svg
                     className="w-6 h-6"
                     fill="currentColor"
                     viewBox="0 0 24 24"
                   >
-                    <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+                    <path d="M19.59 6.69a4.83 4.83 0 01-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 01-5.2 1.74 2.89 2.89 0 012.31-4.64c.298-.002.595.042.88.13V9.4a6.33 6.33 0 00-1-.08A6.34 6.34 0 004 15.66a6.34 6.34 0 0010.95-4.32V6.92a8.32 8.32 0 004.64 1.43V4.9a4.92 4.92 0 01-0.1 1.79z" />
                   </svg>
                 </a>
                 <a
-                  href="https://instagram.com"
+                  href="https://www.instagram.com/bettysorganicaddis?igsh=MTZqOWo5ZzZkc212NA=="
                   className="hover:text-yellow-600"
                 >
                   <svg
@@ -212,7 +212,7 @@ export default function Footer() {
                   </svg>
                 </a>
                 <a
-                  href="https://facebook.com"
+                  href="https://www.facebook.com/bettysproduce?mibextid=ZbWKwL"
                   className="hover:text-yellow-600"
                 >
                   <svg
@@ -221,6 +221,18 @@ export default function Footer() {
                     viewBox="0 0 24 24"
                   >
                     <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
+                  </svg>
+                </a>
+                <a
+                  href="https://www.linkedin.com/company/bettys-produce/"
+                  className="hover:text-yellow-600"
+                >
+                  <svg
+                    className="w-6 h-6"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
                   </svg>
                 </a>
               </div>
