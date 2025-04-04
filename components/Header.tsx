0274e39aafbb1b101bc463bde4007d21e36576de
Yosef-Ali <yosethio@yahoo.com>
2025-04-04 22:41:16 +0300

    feat(Header): remove AuthErrorBoundary wrapper around notification bell

--- components/Header.tsx
+++ components/Header.tsx
@@ -195,11 +195,9 @@ export default function Header({ onMobileMenuToggle, profile }: HeaderProps) {
       {/* Show notification bell for all signed-in users */}
 
       <div className="flex items-center gap-2 mx-2">
-        <AuthErrorBoundary>
-          <div className="relative z-10 transition-all duration-300 hover:scale-110 bg-yellow-50 p-1 rounded-full border border-yellow-200">
-            <NotificationBell />
-          </div>
-        </AuthErrorBoundary>
+        <div className="relative z-10 transition-all duration-300 hover:scale-110 bg-yellow-50 p-1 rounded-full border border-yellow-200">
+          <NotificationBell />
+        </div>
       </div>
 
       <DropdownMenu>
